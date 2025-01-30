431c792dcfb51b064a98bc7fb6c8a8cc740daf85
pbohea <pbohea@gmail.com>
2025-01-30 16:52:36 +0000

    update

--- app.rb
+++ app.rb
@@ -7,12 +7,12 @@ get("/") do
 end
 
 get ("/process_roll") do
-  @num_dice = params.fetch("num_dice").to_i
+  @dice = params.fetch("dice").to_i
   
   @sides = params.fetch("sides").to_i
   @rolls = []
 
-  @num_dice.times do
+  @dice.times do
   die = rand(1..@sides)
 
   @rolls.push(die)
