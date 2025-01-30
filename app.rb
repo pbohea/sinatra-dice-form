618ec4a6de12ac3ae506bab66293100c6e68e199
pbohea <pbohea@gmail.com>
2025-01-30 16:47:26 +0000

    done

--- app.rb
+++ app.rb
@@ -2,17 +2,14 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+
   erb(:home)
 end
 
-get ("/process_roll/:number_of_dice/:how_many_sides") do
-  @num_dice = params.fetch("number_of_dice").to_i
+get ("/process_roll") do
+  @num_dice = params.fetch("num_dice").to_i
   
-  @sides = params.fetch("how_many_sides").to_i
+  @sides = params.fetch("sides").to_i
   @rolls = []
 
   @num_dice.times do
